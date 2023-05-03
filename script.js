77ccd3ac38969edf96ca5d2117b2d343b01fd935
bbiplab165 <bbiplab165@gmail.com>
2023-05-03 13:20:40 +0530

    added

---
+++ script.js
@@ -0,0 +1,13 @@
+const element3 = document.getElementsByClassName("list-group-item");
+element3[2].style.backgroundColor = "green";
+for (let i = 0; i < element3.length; i++) {
+  element3[i].style.fontWeight = "bold";
+}
+
+const tag5=document.getElementsByTagName("li")
+tag5[4].style.border="1px solid lightgray";
+tag5[4].style.padding="10px";
+tag5[4].style.paddingLeft="20px"
+tag5[4].style.backgroundColor="lightgray"
+tag5[4].style.fontWeight="bold"
+
