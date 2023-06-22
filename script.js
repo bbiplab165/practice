f812c5b636d36598916d6f60de26169e88b1f521
bbiplab165 <bbiplab165@gmail.com>
2023-06-22 19:40:28 +0530

    done

--- script.js
+++
@@ -1,5 +0,0 @@
-const element3 = document.getElementsByClassName("list-group-item");
-element3[2].style.backgroundColor = "green";
-for (let i = 0; i < element3.length; i++) {
-  element3[i].style.fontWeight = "bold";
-}
